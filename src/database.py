5fa2bc2070bb3601de93813f85efc3fd1bb43189
nastyaalmaeva <nastyaalmaeva@mail.ru>
2025-06-19 18:35:46 +0300

    Fixed url for database connection

--- src/database.py
+++ src/database.py
@@ -1,13 +1,13 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL", 
-    "postgresql+psycopg://kubsu:kubsu@postgres:5432/kubsu"
+    "postgresql+psycopg://kubsu:kubsu@127.0.0.1:5432/kubsu"
 )
 engine = create_async_engine(DATABASE_URL, echo=True)
-
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
